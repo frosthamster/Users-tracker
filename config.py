590dc3d91b06275fa39835d6e0f706f6c156c0b6
Unknown <frosthamster@gmail.com>
2018-07-06 04:21:30 +0500

    Fixed db standard environment variable

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ from pathlib import Path
 
 class Config:
     SECRET_KEY = os.environ.get('APP_SECRET_KEY') or secrets.token_hex(32)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DB_LOCATION') or f'sqlite:///{Path().joinpath("database.db").absolute()}'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or f'sqlite:///{Path().joinpath("database.db").absolute()}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     USERS_PER_PAGE = 15
